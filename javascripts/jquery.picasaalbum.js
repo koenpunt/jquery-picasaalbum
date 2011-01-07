6ba86121cfdbe11d007906d94b674e516fa5aa79
Koen Punt <koen@koenpunt.nl>
2011-01-07 17:41:06 +0100

    Added new license

--- javascripts/jquery.picasaalbum.js
+++ javascripts/jquery.picasaalbum.js
@@ -2,16 +2,31 @@
  * picasaAlbum - jQuery Plugin
  * Simple plugin for displaying a Picasa Web Album
  * 
- * Copyright (c) 2010 Koen Punt
- *
  * Version: 0.1 (30/12/2010)
- * Build on v1.4 but may work on older jQuery releases 
- *
- * Licensed under Creative Commons:
- *   http://creativecommons.org/licenses/by-sa/3.0/
+ * Build on v1.4 but may work on older jQuery releases
+ * 
+ * Copyright (c) 2010 Koen Punt - http://www.koenpunt.nl/projects/jquery-picasaalbum
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-
 
 (function( $ ){
 	
